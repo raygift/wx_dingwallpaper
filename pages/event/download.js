4115fc4cc541cb21a01c184ad700b19e58711a7d
raygift <the_bigticket@outlook.com>
2017-09-06 15:43:24 +0800

    0.3.4
    "加入对版权问题的判断",
    "下载的图片增加必应logo水印"

--- pages/event/download.js
+++ pages/event/download.js
@@ -5,28 +5,74 @@ Page({
    * 页面的初始数据
    */
   data: {
-    imgUrl: []
+    imgUrl: [],
+    imgs: []
   },
-  startDownload: function (event) {
-    if(this.data.imgUrl==null){
+  Download: function (event) {
+    if (this.data.imgUrl == null) {
       wx.showToast({
         title: '未能加载图片',
         icon: 'error',
         duration: 2000
       })
     }
-    else{
+    else {
       wx.downloadFile({
-        url: "https://cn.bing.com"+this.data.imgUrl,
+        url: "https://cn.bing.com" + this.data.imgUrl,
         // /az/hprichbg/rb/SneffelsRange_ZH-CN9303969066_1920x1080.jpg",
         success: function (res) {
           wx.saveImageToPhotosAlbum({
             filePath: res.tempFilePath,
             success(msg) {
-              wx.showToast({
-                title: '保存成功',
-                icon: 'success',
-                duration: 2000
+              wx.showModal({
+                title: '提示',
+                showCancel: false,
+                content: '壁纸保存成功！',
+                success: function (res) {
+                  if (res.confirm) {
+                    // console.log('用户点击确定')
+                  }
+                }
+              })
+            },
+            fail(err) {
+              console.log(err)
+            },
+            complete(msg) {
+              console.log(msg)
+            }
+          })
+        }
+      })
+    }
+  },
+  Downloadlogo:function(event){
+    if (this.data.imgUrl == null) {
+      wx.showToast({
+        title: '未能加载图片',
+        icon: 'error',
+        duration: 2000
+      })
+    }
+    else {
+      wx.downloadFile({
+        url: "https://cn.bing.com/hpwp/" + this.data.imgs.hsh,
+        success: function (res) {
+          wx.saveImageToPhotosAlbum({
+            filePath: res.tempFilePath,
+            success(msg) {
+              wx.showModal({
+                title: '提示',
+                showCancel:false,
+                content: '壁纸保存成功！',
+                success: function (res) {
+                  if (res.confirm) {
+                    // console.log('用户点击确定')
+                  }
+                  //  else if (res.cancel) {
+                  //   console.log('用户点击取消')
+                  // }
+                }
               })
             },
             fail(err) {
@@ -46,11 +92,22 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.setData({
-      imgUrl: options.id
+    var that = this;
+    wx.request({
+      url: 'https://cn.bing.com/HPImageArchive.aspx', //仅为示例，并非真实的接口地址
+      data: {
+        format: 'js',
+        idx: options.id,
+        n: '1'
+      },
+      success: function (res) {
+        that.setData({
+          imgs: res.data["images"][0],
+          imgUrl: res.data["images"][0]["url"]
+        })
+      }
     })
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
